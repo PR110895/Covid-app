68e1bb477862d1f53f57133df0a76ee96434a371
Pratyush <95492798+PR110895@users.noreply.github.com>
2022-04-19 17:50:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Cards from "./componenet/Cards/Cards";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CountryPicker from "./componenet/CountryPicker/CountryPicker";
+import { fetchData } from "./api";
+import image from "./images/image.png";
+import styles from "./App.module.css";
+
+class App extends React.Component {
+  //State call
+  state = {
+    data: {},
+  };
+
+  //Data call from Api
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    console.log(fetchedData);
+
+    this.setState({ data: fetchedData });
+  }
+  //Country change Handler
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
